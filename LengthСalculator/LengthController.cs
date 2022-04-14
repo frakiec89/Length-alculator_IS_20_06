b96942fe0194815ba5d0bcfa02e9c3559e937dcf
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-04-14 09:19:30 +0400

    Добавьте файлы проекта.

---
+++ LengthСalculator/LengthController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthСalculator
+{
+    internal class LengthController
+    {
+        public List<string> LengthUnit { get; set; } = new List<string>()
+        {
+             "метр" , "километр" , "сантиметр" , "миллиметр"
+        };
+
+        internal object MetematicCalc(string inUint, string outUint, double valueLength)
+        {
+             if (string.Compare(inUint, outUint) == 0)
+                return valueLength;
+
+            if (inUint == "метр" && outUint == "километр") // 1 
+                return valueLength / (double) 1000;
+
+            if (inUint == "метр" && outUint == "сантиметр") //2
+                return valueLength * 100;
+
+
+            if (inUint == "метр" && outUint == "миллиметр") //3
+                return valueLength  * (double)1000;
+
+
+            if (inUint == "километр" && outUint == "метр") //4
+                return valueLength * (double)1000;
+
+
+
+            if (inUint == "километр" && outUint == "сантиметр") //5
+                return valueLength * (double)1000 * 100;
+
+
+            if (inUint == "километр" && outUint == "миллиметр") //6
+                return valueLength * (double)1000 * 100 *100;
+
+
+            return -1;
+
+        }
+    }
+
+
+}
